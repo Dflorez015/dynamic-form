2721fae605e06b4cb7b90f6cd55b6e43dcda65d5
Richard Rodriguez <rrodriguez@comercializadora-s3.com>
2023-11-03 11:18:03 -0500

    - changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve("src", 'App.tsx'),
+      entry: path.resolve(__dirname, 'src/App.tsx'),
       name: 'DynamicForm',
-      fileName: (format) => `DynamicForm.${format}.js`
+      fileName: (format) => `dynamic-form.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
